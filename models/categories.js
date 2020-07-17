1a24dda6b4ef4d8d16152d6c8cbbde95cf23c2c9
Pete Phillips <hermanator123@gmail.com>
2020-07-17 19:10:30 -0400

    updatedToSequelize

---
+++ models/categories.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+    const Category = sequelize.define("category", {
+      // Giving the Author model a name of type STRING
+      category: DataTypes.STRING
+    });
+    Category.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Category.hasMany(models.Product, {
+        onDelete: "cascade"
+      });
+    };
+    return Category;
+  };
