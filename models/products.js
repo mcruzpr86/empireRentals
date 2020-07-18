347011fadc3c45aadcd1f104f27063b0b6c8de7a
Pete Phillips <hermanator123@gmail.com>
2020-07-18 12:14:53 -0400

    sequelizeMaybeFinished

---
+++ models/products.js
@@ -0,0 +1,54 @@
+module.exports = function(sequelize, DataTypes) {
+    var Product = sequelize.define("products", {
+      item_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      item_description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate:{
+        len: [1]
+      }},
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+        len: [1]
+      }},
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate:{
+        len: [1]
+      }},
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+        len: [1]
+      }},
+      imgurl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+        len: [1]
+      }}
+      
+    });
+  
+    Product.associate = function(models) {
+     
+      Product.belongsTo(models.Category, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Product;
+  };
+  
