e8073a38065e5e270dd92f773eb44d7b8ce8d4c1
Pete Phillips <hermanator123@gmail.com>
2020-07-23 11:57:48 -0400

    images and price put into each card

--- public/js/vehicles.js
+++ public/js/vehicles.js
@@ -3,11 +3,15 @@ $.get("/api/products/category/2", function(data) {
 
     data.Products.forEach((product, index) => {
         const productCard = $(`#card${index}`)
+        const productImg = $(productCard).find("img.productImg")
+        productImg.attr("src", product.imgurl)
         const productTitle = $(productCard).find("h5")
         productTitle.text(product.item_name)
         const description = $(productCard).find("p.product-description")
         description.text(product.item_description)
         const cartButton = $(productCard).find(".cartBtn")
         cartButton.attr("data-cartbutton", index)
+        const productPrice =  $(productCard).find("span")
+        productPrice.text(product.price)
     });
 })
